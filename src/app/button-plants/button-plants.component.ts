a58f213e5c5b2e18f678f782725394445a338339
JaimeNM0 <navarro.m.jai@gmail.com>
2024-01-18 17:52:46 +0100

    Se han modificado las partes de button-plants para crear su formato del componente y form-plants para poner todas las etiquetas del formulario en el html.

--- src/app/button-plants/button-plants.component.ts
+++ src/app/button-plants/button-plants.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-plants',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './button-plants.component.css'
 })
 export class ButtonPlantsComponent {
-
+  @Input() icon!: string;
 }
--- src/app/form-plants/form-plants.component.ts
+++ src/app/form-plants/form-plants.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { ComponetPlantsComponent } from '../componet-plants/componet-plants.component';
 import { CommonModule } from '@angular/common';
 import { PlantSelection } from '../models/plant-selection.interface';
+import { ButtonPlantsComponent } from '../button-plants/button-plants.component';
 
 @Component({
   selector: 'app-form-plants',
   standalone: true,
-  imports: [ComponetPlantsComponent, CommonModule],
+  imports: [ComponetPlantsComponent, CommonModule, ButtonPlantsComponent],
   templateUrl: './form-plants.component.html',
   styleUrl: './form-plants.component.css'
 })
 export class FormPlantsComponent {
+  public aceptar: string = "../../../assets/img/icono_aceptar.png";
+  public cancelar: string = "../../../assets/img/icono_cancelar.png";
   public plantsSelection: PlantSelection[] = [
     {
       name: 'Arbusto',
